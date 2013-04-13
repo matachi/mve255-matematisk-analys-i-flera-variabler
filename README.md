24e887aee442655d2f83eacd38a8564c0407aabb
MaTachi <dannie92@gmail.com>
2013-04-13 20:37:00 +0200

    Implemented newton.m

--- README.md
+++ README.md
@@ -4,3 +4,27 @@ MVE255, Matematisk analys i flera variabler M, 2012/13
 Daniel Jonsson, IT, Chalmers
 
 [Course site](http://www.math.chalmers.se/Math/Grundutb/CTH/mve255/1213/)
+
+Exercise 2
+----------
+
+    >> jacobi(@funk2, [1;2])
+
+    ans =
+
+       -4.0000         0
+       -2.0000   -1.0000
+
+    >> newton(@funk2, [1;1], 0.1)
+
+    ans =
+
+        1.0000
+        2.0000
+
+    >> newton(@funk2, [-2;-1], 10^-6)
+
+    ans =
+
+       -1.0000
+       -2.0000
---
+++ exercise2/newton.m
@@ -0,0 +1,53 @@
+function x = newton(f,x0,tol)
+% Newton's method for the system of equations f(x)=0
+% 
+%   Syntax: 
+%           x = newton(f,x0,tol)
+%   Arguments: 
+%           f - function handle pointing to a function file  
+%               which returns an nx1 matrix y=f(x).  
+%           x0 - an nx1 matrix, the initial approximation
+%           tol - a positive number, the tolerance 
+%   Returns: 
+%           x - an nx1 matrix, an approximate solution 
+%   Description:
+%           The program newton uses Newton's method to compute an 
+%           approximate solution of the system of equations f(x)=0.  
+%           The file f.m must return an nx1 matrix y=f(x).  
+%           The Jacobi matrix A=Df(x) is computed by the subfunction
+%           jacobi.m below.  
+%           Here x, x0, y are of type nx1 and A is of type nxn.
+%           If the initial approximation x0 is sufficiently close 
+%           to an exact root x_exact, then the program
+%           computes an approximate solution x with the error
+%           |x-x_exact| < tol.  
+%   Examples: 
+%          x = newton(@sin, 3, 1e-7) computes pi to 7 decimals
+%
+%          If the m-file funk1.m contains 
+%          function y = funk1(x)
+%          y=x.^2-2;
+%
+%          then the command 
+%          x = newton(@funk1, 3, 1e-7) 
+%          computes the square root of 2. 
+%   See also:
+%          bisec.m, fixpoint.m
+%
+
+%----------------------------------------------------------------------
+%   Date created: 
+%
+%   Author:  
+%------------------------------+---------------------------------------+
+h = 1;
+n = length(x0);
+x = x0;
+
+while abs(h) > tol
+    A = jacobi(f, x0);
+    b = -f(x);
+    h = A\b;
+    x = x+h;
+end
+
