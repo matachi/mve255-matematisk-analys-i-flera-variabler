4e168bfbf3d6d590b3ae34b52f16af1d23eb7b9e
MaTachi <dannie92@gmail.com>
2013-04-24 20:47:49 +0200

    Fix a word

--- exercise3/extremeFinder.m
+++ exercise3/extremeFinder.m
@@ -3,6 +3,6 @@ function [ p, e ] = extremeFinder( f, x0, tol )
 gradient = @(x)(jacobi(f, x)'); % gradient
 p = newton(gradient, x0, tol);  % values when gradient is zero
 h = jacobi(gradient, p);        % hesse matrix
-e = eig(h);                     % egenv‰rden
+e = eig(h);                     % egenv√§rden
 end
 
