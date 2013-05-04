9ab9492710178589efc802d954d90772c690be57
MaTachi <dannie92@gmail.com>
2013-05-04 15:54:14 +0200

    Update code for Ex4 Prob4

--- exercise4/EqData4.m
+++ exercise4/EqData4.m
@@ -1,20 +1,16 @@
 function [a, d, c, f] = EqData4(x, tag)
-% Returns the values at x of the data a=a(x), d=d(x), c=c(x) and f=f(x) 
-% in the differential equation D(-aDu)+dDu+cu=f.
-% tag is a subdomain reference tag, which is not used in this example.
-% 
-
-a = 0;
-if x < 1/2
-    a=7;
-elseif x > 1/2
-    a=22;
+E=1e7;      % N/m^2 Young's modulus
+A=1;        % m^2 area
+p=1e3;      % kg/m^3
+% E: https://en.wikipedia.org/wiki/Young%27s_modulus#Approximate_values
+% p: http://en.wikipedia.org/wiki/Densities_of_the_elements_(data_page)
+if x <= 1/2 % Aluminum
+    E = E * 7;
+    p = p * 3;
+elseif x > 1/2 % Iron
+    E = E * 22;
+    p = p * 8;
 end
-d=0;
-c=0;
-f = 0;
-if x < 1/2
-    f=3;
-elseif x > 1/2
-    f=8;
-end
+a=E*A;
+f=p;
+d=0; c=0;
